155a2b1e62ff46ee5806b875484e2030e999fcd0
anant-sachdeva <anantsachdeva@live.com>
2016-11-18 18:53:33 +0530

    added a couple files about numbers and UTF-8 encoding

---
+++ 02_numbers/42.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("representing number 42.. \ndecimal %d \nbinary %b\nhexadecimal %x\nHEX %X\noctal %o\ncharacter %c\n AND THIS ALL IS %B",42,42,42,42,42,42,true)
+
+
+}
+
+
+
+
+
+
